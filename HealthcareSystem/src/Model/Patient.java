73d6cd62d926f59d909bf3eb6fbe5994d0b2c978
Bhagyadilhara <bhagyadilhara123@gmail.com>
2020-04-14 14:50:36 +0530

    Added insertPatient function with POST method

--- HealthcareSystem/src/Model/Patient.java
+++ HealthcareSystem/src/Model/Patient.java
@@ -1,5 +1,52 @@
 package Model;
 
+import java.sql.*;
+
+import DbConnect.DbConnect;
+
 public class Patient {
 
+	//insert-------------------------------------------------------------------------------------------------------------------------
+		public String insertPatient(String username,String address,String nic,String dob,String gender,String mobile_number,String email,String password)
+	    {
+				String output = "";
+				try
+				{
+					DbConnect db = new DbConnect();
+					Connection con = null;
+					con = db.connect();
+					
+					if (con == null)
+					{    
+						return "Error while connecting to the database for inserting new patient.";
+					}
+					// create a prepared statement
+					String query = "insert into patient"
+							+"(`userID`,`userName`,`userAddress`,`nicNo`,`dateOfBirth`,`gender`,`mobileNumber`,`email`,`password`)"
+							 + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+							PreparedStatement preparedStmt = con.prepareStatement(query); 
+					// binding values
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, username);
+					preparedStmt.setString(3, address);
+					preparedStmt.setString(4, nic);
+					preparedStmt.setString(5, dob);
+					preparedStmt.setString(6, gender); 
+					preparedStmt.setString(7, mobile_number); 
+					preparedStmt.setString(8, email); 
+					preparedStmt.setString(9, password); 
+		
+					// execute the statement
+					preparedStmt.execute();
+					con.close();
+					output = "Inserted new patient successfully";
+				}
+					catch (Exception e)
+				{
+						output = "Error while inserting the new patient!";
+						System.err.println(e.getMessage());
+				}
+				return output;
+	    		}
+	
 }
--- HealthcareSystem/src/Services/PatientService.java
+++ HealthcareSystem/src/Services/PatientService.java
@@ -1,5 +1,40 @@
 package Services;
 
-public class PatientService {
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
 
+import Model.Patient;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+
+@Path("/Patient")
+public class PatientService {
+	
+//Item itemObj = new Item();
+	Patient patientObj = new Patient();
+	
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPatient(@FormParam("userName") String username,@FormParam("userAddress") String address,
+			@FormParam("nicNo") String nic,@FormParam("dateOfBirth") String dob,@FormParam("gender") String gender,
+			@FormParam("mobileNumber") String mobile_number,@FormParam("email") String email,@FormParam("password") String password){
+	
+		String output = patientObj.insertPatient(username, address, nic, dob, gender, mobile_number, email, password);
+		
+		return output;
+	}
+	
+	
+	
 }
