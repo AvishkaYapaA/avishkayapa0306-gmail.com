9fcf176bfe6508703d8942ae03cd4d0f41da4e9e
PRADEEP <tharangapradepha@gmail.com>
2020-04-14 17:51:12 +0530

    Insert part successfully

--- HealthcareSystem/src/Model/Hospital.java
+++ HealthcareSystem/src/Model/Hospital.java
@@ -1,5 +1,55 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import DbConnect.DbConnect;
+
 public class Hospital {
 
+	//insert hospitals-------------------------------------------------------------------------------------------------------------------------
+	
+			public String insertHospital(String MOHCode,String Managername,String Hospitalname,String address,String TPNumber,String location,String username,String password)
+		    {
+					String output = "";
+					try
+					{
+						DbConnect db = new DbConnect();
+						Connection con = null;
+						con = db.connect();
+						
+						if (con == null)
+						{    
+							return "Error while connecting to the database for inserting new hospital.";
+						}
+						// create a prepared statement
+						String query = "insert into hospital"
+								+"(`HospitalID`,`MOHcode`,`ManagerName`,`HospitalName`,`Address`,`TPnumber`,`Location`,`Username`,`Password`)"
+								 + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+								PreparedStatement preparedStmt = con.prepareStatement(query); 
+						// binding values
+						preparedStmt.setInt(1, 0);
+						preparedStmt.setString(2, MOHCode);
+						preparedStmt.setString(3, Managername);
+						preparedStmt.setString(4, Hospitalname);
+						preparedStmt.setString(5, address);
+						preparedStmt.setString(6, TPNumber); 
+						preparedStmt.setString(7, location); 
+						preparedStmt.setString(8, username); 
+						preparedStmt.setString(9, password); 
+			
+						// execute the statement
+						preparedStmt.execute();
+						con.close();
+						output = "Inserted new hospital successfully";
+					}
+						catch (Exception e)
+					{
+							System.out.println("erro insert hospital");
+							output = "Error while inserting the new hospital!";
+							System.err.println(e.getMessage());
+					}
+					return output;
+		    		}
+			
 }
--- HealthcareSystem/src/Services/HospitalService.java
+++ HealthcareSystem/src/Services/HospitalService.java
@@ -1,5 +1,39 @@
 package Services;
 
+//For REST Service
+import javax.ws.rs.*;
+
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+import Model.Hospital;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+
+
+@Path("/Hospital")
 public class HospitalService {
+	
+	
+	Hospital hospitalObj = new Hospital();
+		
+		
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertHospital(@FormParam("MOHcode") String MOHCode,@FormParam("ManagerName") String Managername,
+				@FormParam("HospitalName") String Hospitalname,@FormParam("Address") String address,@FormParam("TPnumber") String TPNumber,
+				@FormParam("Location") String location,@FormParam("Username") String username,@FormParam("Password") String password){
+		
+			String output = hospitalObj.insertHospital(MOHCode, Managername, Hospitalname, address, TPNumber, location, username, password);
+			
+			return output;
+		}
 
 }
